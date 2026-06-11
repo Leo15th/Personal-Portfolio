6a5e0ed18edd3e3a823fa03affc624eaba98ae46
Leo15th <pwaiag2023@gmail.com>
2026-06-11 11:45:34 +0700

    change base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Personal-Portfolio/",
+  // base: "/Personal-Portfolio/",
+  base: "/",
   plugins: [
     react(),
     tailwindcss(),
